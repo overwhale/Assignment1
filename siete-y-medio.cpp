5e6c862ee6148803cef4c2f46febf61789aaea20
Iris <irisliu203@gmail.com>
2018-06-04 19:04:09 -0700

    The fstream doesn't really work as expected.

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -18,96 +18,97 @@ int main(){
     while(!p1.isEmpty() && !dealer.isEmpty()){
 		Hand h1;
 		Hand h2;
-		cout << "you have $" << p1.get_money() << ". enter bet: ";
+		//ostream std::std::cout;
+		std::cout << "you have $" << p1.get_money() << ". enter bet: ";
 		cin>> bet;
-    	cout << "Your cards:"<< endl;
+    	std::cout << "Your cards:"<< endl;
 		h1.add_card(Card());
-		cout << "Your total is: " << setprecision(2) << h1.get_value() <<
+		std::cout << "Your total is: " << setprecision(2) << h1.get_value() <<
 			". Do you want another card (y/n)? ";
 		char ans;
 		cin >> ans;
 
-//		cout << "The dealer's total is " << setprecision(2) << h2.get_value();
+//		std::cout << "The dealer's total is " << setprecision(2) << h2.get_value();
 
 		while (ans == 'y') {
 		//	h2.add_card();
-			cout << "New Card£º " << endl;
+			std::cout << "New Card£º " << endl;
 			h1.add_card(Card());
-			cout << "Your cards:" << endl;
+			std::cout << "Your cards:" << endl;
 			h1.show_all();
-			cout << "Your total is: " << setprecision(2) << h1.get_value() <<"." << endl;
+			std::cout << "Your total is: " << setprecision(2) << h1.get_value() <<"." << endl;
 			if (h1.get_value() < 7.5) {
-				cout << " Do you want another card (y/n)? ";
+				std::cout << " Do you want another card (y/n)? ";
 				cin >> ans;
 			}else {
 				ans = 'n';
 			}
 		}
 		// Dealer gets first card.
-		cout << "Dealer's cards:"<< endl;
+		std::cout << "Dealer's cards:"<< endl;
 		h2.add_card(Card());
-		cout << "The dealer's total is " << setprecision(2) << 
+		std::cout << "The dealer's total is " << setprecision(2) << 
 				h2.get_value()<<"."<<endl;
 
 		// check if dealer needs to get card
 		while (h2.get_value() < 5.5) {
-			cout << "New Card£º " << endl;
+			std::cout << "New Card£º " << endl;
 			h2.add_card(Card());
-			cout << "Dealer's cards:" << endl;
+			std::cout << "Dealer's cards:" << endl;
 			h2.show_all();
-			cout << "The dealer's total is " << setprecision(2) << 
+			std::cout << "The dealer's total is " << setprecision(2) << 
 				h2.get_value() <<"." <<endl;
 		}
 		// Result comparison
-		cout << endl;
+		std::cout << endl;
 		if (h1.get_value()> 7.5 ) {
-			cout << "You lose:" << bet << "!" << endl;
+			std::cout << "You lose:" << bet << "!" << endl;
 			p1.lose(bet);
 			dealer.win(bet);
 		}
 		else if (h2.get_value() > 7.5) {
-			cout << "You win:" << bet << "!" << endl;
+			std::cout << "You win:" << bet << "!" << endl;
 			p1.win(bet);
 			dealer.lose(bet);
 		}
 		else if (h1.get_value() < h2.get_value()) {
-			cout << "You lose:" << bet << "!" << endl;
+			std::cout << "You lose:" << bet << "!" << endl;
 			p1.lose(bet);
 			dealer.win(bet);
 		}
 		else if (h1.get_value() == h2.get_value()) {
-			cout << "Nobody wins!" << endl;
+			std::cout << "Nobody wins!" << endl;
 		}
 		else {
-			cout << "You win:" << bet << "!" << endl;
+			std::cout << "You win:" << bet << "!" << endl;
 			p1.win(bet);
 			dealer.lose(bet);
 		}
 
-		ofstream file;
-		file.open("Gamelog.txt");
-		if (file.is_open()) {
-			file << endl;
-			file << "----------------------------------------------" << endl;
-			file << "Game number: " << num << "        " << "Money left: $" <<
-				p1.get_money()<<endl;
-			file << "Bet: " << bet<<endl;
-			file << "Your cards:" << endl;
-			h1.show_all();
-			file << "Your total is: " << setprecision(2) << h1.get_value() << "." << endl<<endl;
-			file << "Dealer's cards:" << endl;
-			h2.show_all();
-			file << "The dealer's total is " << setprecision(2) <<
-				h2.get_value() << "." << endl<<endl;
+		//ofstream file;
+		//file.open("Gamelog.txt", std::ofstream::out | std::ofstream::app);
+		//if (file.is_open()) {
+		//	file << endl;
+		//	file << "----------------------------------------------" << endl;
+		//	file << "Game number: " << num << "        " << "Money left: $" <<
+		//		p1.get_money()<<endl;
+		//	file << "Bet: " << bet<<endl;
+		//	file << "Your cards:" << endl;
+		//	h1.show_all();
+		//	file << "Your total is: " << setprecision(2) << h1.get_value() << "." << endl<<endl;
+		//	file << "Dealer's cards:" << endl;
+		//	h2.show_all();
+		//	file << "The dealer's total is " << setprecision(2) <<
+		//		h2.get_value() << "." << endl<<endl;
 
-			file << "----------------------------------------------" << endl;
-		}
-		file.close();
+		//	file << "----------------------------------------------" << endl;
+		//}
+		//file.close();
 
 		// Reset hands
 		h1.reset();
 		h2.reset();
-		cout << "=============================================" << endl;
+		std::cout << "=============================================" << endl;
 		num++;
     }
 	system("pause");
